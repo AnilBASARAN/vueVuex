1767406403f22c0c0944aede12e183a72c5b591a
AnilBASARAN <nlbsrn@gmail.com>
2025-06-03 04:08:38 +0300

    mapGetters explained

--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ const store = createStore({
   actions: {
     //its just a method
     increment(context) {
+      console.log(context);
       setTimeout(function () {
         context.commit('increment');
       }, 2000);
+    },
+    increase(context, payload) {
+      console.log(context);
+      context.commit('increase', payload);
     },
   },
   state() {
