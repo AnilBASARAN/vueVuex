f256e75fbcfbdf233b231c90b927eec603c381a1
AnilBASARAN <nlbsrn@gmail.com>
2025-06-03 04:23:19 +0300

    map actions name change is cool to know

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,16 +1,23 @@
 <template>
-  <button @click="addTwo">Add 2</button>
+  <button @click="increase({ value: 15 })">Add 15</button>
+  <button @click="inc">Add me 1</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   methods: {
-    addTwo() {
+    /*     addTwo() {
       this.$store.commit({
         type: 'increment',
         value: 2,
       });
-    },
+    }, */
+    ...mapActions({
+      inc: 'increment',
+      increase: 'increase',
+    }),
   },
 };
 </script>
