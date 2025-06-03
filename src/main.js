273734757e7372fbfe26adf26f0e42adb45dd0bd
AnilBASARAN <nlbsrn@gmail.com>
2025-06-03 13:59:55 +0300

    organizing your store with modules

--- src/main.js
+++ src/main.js
@@ -3,7 +3,60 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const counterModule = {
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth;
+    },
+    increment(state) {
+      state.counter = state.counter + 1;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+    setToZero(state) {
+      state.counter = 0;
+    },
+  },
+  actions: {
+    //its just a method
+    increment(context) {
+      console.log(context);
+      setTimeout(function () {
+        context.commit('increment');
+      }, 2000);
+    },
+    increase(context, payload) {
+      console.log(context);
+      context.commit('increase', payload);
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    },
+  },
+};
+
 const store = createStore({
+  modules: {
+    numbers: counterModule,
+  },
   actions: {
     login(context) {
       context.commit('setAuth', { isAuth: true });
